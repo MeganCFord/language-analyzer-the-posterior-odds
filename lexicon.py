566f818c477ab0f16a92c8e5e8c182c48f697ef1
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-07-18 16:08:30 -0500

    Added disgust and anger words to lexicon.

--- lexicon.py
+++ lexicon.py
@@ -13,15 +13,18 @@ lexicon = [
     {
       "disgust": {
         "positive_words": [],
-        "negative_words": []
+        "negative_words": ["idiot", "sick", "annoy", "bad", "gross", "disgust", "awful", "vomit",
+          "cannibal", "carcass", "rot", "dislike", "hate", "worthless", "weird", "ugly", "trash",
+          "toxic", "terrible", "vulgar"]
       },
       "fear": {
         "positive_words": [],
         "negative_words": []
       },
       "anger": {
-        "positive_words": [],
-        "negative_words": []
+        "positive_words": ["yell", "awful", "insult"],
+        "negative_words": ["hate", "anger", "angry", "mad", "enemy", "yell", "scream", "wrong", "wreck", "awful",
+          "terrible", "idiot", "stupid", "dumb", "hostile", "insult", "resent"]
       },
       "happy": {
         "positive_words": [],
