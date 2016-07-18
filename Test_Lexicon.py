161d90a6376e53930b70f8d83a732fafc00b5919
Megan Ford <meg.c.ford@gmail.com>
2016-07-18 16:02:11 -0500

    eod update with all needed tests noted in either comment or name- except behaviorizer, not sure what to do there

--- Test_Lexicon.py
+++ Test_Lexicon.py
@@ -1,3 +1,105 @@
-import nltk
+
 import messages
 import unittest
+
+
+class Test_analyzer_creation(unittest.TestCase):
+
+    def test_that_analyzer_argument_is_string(self):
+
+    def test_that_tokenizer_is_instantiated(self):  # isInstanceOf tokenizer
+
+    def test_that_argument_and_initial_sentence_property_are_equal(self):
+
+    def test_printer_of_string_sentence(self):
+
+
+class Test_tokenizer(unittest.TestCase):
+
+    def test_that_domainer_is_instantiated(self):
+
+    def test_that_emotionizer_is_instantiated(self):
+
+    def test_what_it_receives_is_the_initial_sentence_property(self):
+
+    def test_what_it_outputs_is_a_list(self):
+
+    # we also need tests in here that note the word count, character count, word placement
+
+
+
+class Test_emotionizer(unittest.TestCase):
+
+    def test_that_sentimentizer_was_created(self):
+
+    def test_what_it_receives_is_a_list(self):
+
+    def test_that_it_can_connect_to_lexicon(self):
+
+    def test_that_lexicon_is_the_thing(self):
+
+    def test_that_each_emotion_includes_sentiments(self):
+
+
+    # counter start tests
+    def test_that_happy_counter_starts_at_zero(self):
+
+    def test_that_sad_counter_starts_at_zero(self):
+
+    def test_that_angry_counter_starts_at_zero(self):
+
+    def test_that_disgust_counter_starts_at_zero(self):
+
+    def test_that_fear_counter_starts_at_zero(self):
+
+    # TODO: loop test with counter end tests (test that match adds one, that no match does not add one)
+    # TODO: math test to divide each non-zero counter against total num of accrued points.
+    # TODO: print the decimal value of each non-zero counter.
+    # TODO: find the highest non-zero value(s)
+    # TODO: send highest value(s) to the behaviorizer (somehow)
+
+class Test_sentimentizer(unittest.TestCase):
+
+    # counter start tests
+    def test_that_positive_counter_starts_at_zero(self):
+
+    def test_that_negative_counter_starts_at_zero(self):
+    # TODO: loop through the emotion counters and test that match adds one, that no match does not add one)
+    # TODO: math test to subtract neg from pos and divide result by total num of accrued points.
+    # TODO: print the decimal value
+    # TODO: send the value to the behaviorizer
+
+class Test_domainer(unittest.TestCase):
+
+    def test_what_it_receives_is_a_list(self):
+
+    def test_that_it_can_connect_to_lexicon(self):
+
+    def test_that_lexicon_is_a_list(self):
+
+    # counter start tests
+    def test_that_financial_counter_starts_at_zero(self):
+
+    def test_that_behavioral_counter_starts_at_zero(self):
+
+    def test_that_scientific_counter_starts_at_zero(self):
+
+    def test_that_educational_counter_starts_at_zero(self):
+
+    def test_that_politics_counter_starts_at_zero(self):
+
+    def test_that_relationships_counter_starts_at_zero(self):
+
+    # TODO: loop test with counter end tests (match adds a point, no match does not add a point)
+    # TODO: math test to divide each non-zero counter against total num of accrued points.
+    # TODO: print the decimal value of each non-zero counter.
+    # TODO: send the highest value(s) to the behaviorizer
+
+class Test_behaviorizer(unittest.TestCase):
+
+    def test_what_it_receives(self):  # going to be receiving a bunch of little tuples or something.
+
+
+if __name__ == '__main__':
+    unittest.main()
+
