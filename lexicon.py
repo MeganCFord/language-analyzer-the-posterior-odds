d456867f95eca105aab370e525b4febcbc8f94b6
Melanie Baker <melanierbaker@yahoo.com>
2016-07-19 09:52:25 -0500

    added fear words to lexicon

--- lexicon.py
+++ lexicon.py
@@ -1,11 +1,5 @@
 lexicon = [
   {
-    "sentiment":
-    {
-      "positive": [],
-      "negative": [],
-      "neutral": []
-    },
     "domain":
     {
       "financial": [],
@@ -15,13 +9,33 @@ lexicon = [
       "politics": [],
       "relationships": []
     },
-    "behavior":
+    "emotion":
     {
-      "aggressive": [],
-      "passive": [],
-      "mentoring": [],
-      "inquisitive": [],
-      "transaction": []
+      "disgust": {
+        "positive_words": [],
+        "negative_words": ["idiot", "sick", "annoy", "bad", "gross", "disgust", "awful", "vomit",
+          "cannibal", "carcass", "rot", "dislike", "hate", "worthless", "weird", "ugly", "trash",
+          "toxic", "terrible", "vulgar"]
+      },
+      "fear": {
+        "positive_words": ["alertness", "armed", "attorney", "birth", "cash", "cautious", "compassion",
+        "defend", "destination", "flying", "holiness"],
+        "negative_words": ["abandon", "abduction", "absence", "abyss", "accident", "afraid", "wrath",
+        "victim", "unknown", "tyrant", "terrorize", "surveillance", "superstitious"]
+      },
+      "anger": {
+        "positive_words": ["yell", "awful", "insult"],
+        "negative_words": ["hate", "anger", "angry", "mad", "enemy", "yell", "scream", "wrong", "wreck", "awful",
+          "terrible", "idiot", "stupid", "dumb", "hostile", "insult", "resent"]
+      },
+      "happy": {
+        "positive_words": [],
+        "negative_words": []
+      },
+      "sad": {
+        "positive_words": [],
+        "negative_words": []
+      }
     }
   }
 ]
