6c8e7767a8d2e68b85725d2f3f6baf8e70e38414
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-07-20 15:44:27 -0500

    Added stemming to Spokenizer, then refactored code and tests, so logic happens in parse_sentence function, rather than init function

--- Spokenizer.py
+++ Spokenizer.py
@@ -1,14 +1,25 @@
 from Posterior import *
 import nltk
+from nltk.stem.porter import *
 
 class Spokenizer:
 
     def __init__(self, sentence):
-        self.sentence = sentence.lower()
-        self.list = nltk.word_tokenize(sentence)
 
-        # self.Domainizer = Domainizer()
+        self.raw_sentence = sentence
+        self.parsed_sentence = self.parse_sentence()
 
+    def parse_sentence(self):
 
-# import code
-# code.interact(local=locals())
+        stemmer = PorterStemmer()
+        sentence = self.raw_sentence.lower()
+
+        list = nltk.word_tokenize(sentence)
+
+        self.stemmed_list = [stemmer.stem(l) for l in list]
+        return self.stemmed_list
+
+        # self.domainer = Domainer(stemmed_list)
+        # self.emotionizer = Emotionizer(stemmed_list)
+        # self.sentimentizer = Sentimentizer(stemmed_list)
+
--- Test_Spokenizer.py
+++ Test_Spokenizer.py
@@ -3,24 +3,33 @@ import nltk
 from Posterior import *
 from Spokenizer import *
 
+# whatever these will be called, they need importing
+# from Domainer import *
+# from Emotionizer import *
+# from Sentimentizer import *
+
 class Test_Spokenizer(unittest.TestCase):
+    # Can't test individually for lowercase-ing and list-ifying.
+    # Can only test the final output, being sent to the other modules.
 
-    def test_sentence_converts_to_lower(self):
-        test_sentence = Spokenizer("This is A sentence.")
-        self.assertEqual(test_sentence.sentence, "this is a sentence.")
+    def test_stemmed_list_returns_stemmed_lowercase_word_list(self):
+        phrase = Spokenizer("Baked A cake, and Eating it tomorrow")
+        self.assertEqual(phrase.parse_sentence(), ['bake', 'a', 'cake', ',', 'and', 'eat', 'it', 'tomorrow'])
 
-    def test_nltk_tokenized_sentence_outputs_a_list(self):
+    def test_that_domainer_property_was_initialized(self):
+        # test_spokenizer = Spokenizer("Duh, a sentence")
+        # self.assertIsInstance(test_spokenizer.domainer, Domainer)
         pass
 
-    def test_that_domainer_was_created(self):
+    def test_that_emotionizer_property_was_initialized(self):
+        # test_spokenizer = Spokenizer("Here's another!!")
+        # self.assertIsInstance(test_spokenizer.emotionizer, Emotionizer)
         pass
 
-    def test_that_emotionizer_was_created(self):
+    def test_that_sentimentizer_property_was_initialized(self):
+        # test_spokenizer = Spokenizer("What up, language?")
+        # self.assertIsInstance(test_spokenizer.sentimentizer, Spokenizer)
         pass
-
-    def test_that_sentimentizer_was_created(self):
-        pass
-
 
 if __name__ == '__main__':
     unittest.main()
