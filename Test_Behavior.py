1d7cbc6a9c5749e0973eb3d4a4ad7c6f414af717
Megan Ford <meg.c.ford@gmail.com>
2016-07-20 11:18:15 -0500

    corrected json. prints. other stuff does not work

--- Test_Behavior.py
+++ Test_Behavior.py
@@ -13,17 +13,32 @@ class Test_posterior(unittest.TestCase):
         self.assertIsInstance(self.sentence_one.initial, str)
         self.assertEqual("This is a test sentence", self.sentence_one.initial)
 
+        # what if I just stringified everything in the argument, to make sure?
         with self.assertRaises(TypeError):
             sentence_bad = Posterior(2)
             sentence_also_bad = Posterior(set())
 
-    # these tests will fail until combined with teammate work.
+
+    # this test will fail until combined with teammate work.
     def test_that_spokenizer_is_instantiated(self):
         self.assertIsInstance(self.sentence_one.spokenizer, Spokenizer())
 
+    def test_that_behaviorizer_is_instantiated(self):
+        self.assertIsInstance(self.sentence_one.predicted_behavior, Behaviorizer())
+
 
 class Test_behaviorizer(unittest.TestCase):
-    pass
+
+    @classmethod
+    def setUpClass(self):
+        self.sentence_json = Behaviorizer("positive", "happy", "scientific")
+
+    def test_json_load(self):
+    # TODO: test the loading of the json.
+    # TODO: test that the json is traversable.
+    # TODO: test that we're passing in a tuple.
+    # TODO: test that the tuples will match up.
+        pass
 
 if __name__ == '__main__':
     unittest.main()
--- behaviorizer.py
+++ behaviorizer.py
@@ -3,8 +3,11 @@ class Posterior:
     def __init__(self, string):
         
         self.initial = string
+        # TODO: handle if it's not a string. maybe str()?
+
         self.spokenizer = Spokenizer(Posterior.initial)
 
+        # TODO: rename these three finders to what teammates name their find-highest-variable functions.
         self.master_domain = Domainizer.find_master()
         self.master_emotion = Emotionizer.find_master()
         self.master_sentiment = Sentimentizer.find_master()
@@ -12,22 +15,33 @@ class Posterior:
         self.predicted_behavior = Behaviorizer(Posterior.master_domain, Posterior.master_emotion, Posterior.master_sentiment)
 
 class Behaviorizer:
-    import json
-    from pprint import pprint
 
 
     def __init__(self, domain, emotion, sentiment):
 
-        self.peram_set = tuple(domain, emotion, sentiment)
-        self.prediction = find_prediction(peram_set)
+        self.behavior_list = self.import_json()
+        # TODO: make the three perameters a tuple
+        self.peram_set = set(domain, emotion, sentiment)
+        self.peram_tuple = tuple(self.peram_set)
+        self.prediction = self.find_prediction(self.peram_set)
 
+
+    def import_json(self):
+        # TODO: write docstring here.
+        import json
         # loads json of potential behaviors (I think).
         with open('Behavior_List.json') as data_file:
             behaviors = json.load(data_file)
-            pprint(behaviors)
+            print(behaviors['Behaviors'][0])
 
-    def find_prediction(domain, emotion, sentiment):
+    def find_prediction(self, masters):
+        # TODO: write docstring here.
         # loop through behavior list somehow.
-        # return a thing from the lexicon.
+        # return a string from the behaviors lexicon.
+        # print the returned value.
         pass
 
+
+#questions:
+# do I need any other docstrings besides find_prediction?
+# 
