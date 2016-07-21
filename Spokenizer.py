19be2d517b68bdffabfa602a3f22003850f9b7f4
whitney-mitchell <whitney.mitchell84@gmail.com>
2016-07-21 09:34:14 -0500

    Added doc strings.

--- Spokenizer.py
+++ Spokenizer.py
@@ -5,7 +5,17 @@ from nltk.stem.porter import *
 class Spokenizer:
 
     def __init__(self, sentence):
+        '''
+        This function initializes the Spokenizer object,
+        receiving a string of words, assigning it as a variable,
+        and sending it to the parse_sentence function.
+        The resulting list of words is saved here as parsed_sentence,
+        and passed to the Domainer, Sentimentizer, and Emotionizer.
 
+        Arguments: a string of words.
+        Example: "I am not a cheesy example sentence."
+
+        '''
         self.raw_sentence = sentence
         parsed_sentence = self.parse_sentence()
 
@@ -14,7 +24,12 @@ class Spokenizer:
         self.sentimentizer = Sentimentizer(parsed_sentence)
 
     def parse_sentence(self):
+        '''
+        This function receives the sentence variable, tokenizes it
+        into a list of words, changes any uppercase letters to
+        lowercase, and then removes suffixes from the words.
 
+        '''
         stemmer = PorterStemmer()
 
         list = nltk.word_tokenize(self.raw_sentence.lower())
