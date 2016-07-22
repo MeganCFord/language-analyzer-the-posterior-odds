bdd67362fb6c7e9593dbddd1e671142c7bf0238c
Danielle Scheele <strangeyellowbird@gmail.com>
2016-07-22 09:40:50 -0500

    added arguments: none to my docstring

--- domainer.py
+++ domainer.py
@@ -23,6 +23,8 @@ class Domainer:
     def total_all_domain_points(self):
         """
         This function tallies all of the domain counters and stores the total in the self.total variable.
+
+        arguments: none
         """
         self.total = self.financial_total + self.behavioral_total + self.scientific_total + self.educational_total + self.political_total + self.relationships_total
 
@@ -32,6 +34,8 @@ class Domainer:
     def financial_counter(self):
         """
         The financial_counter adds a 1 for each word in the list passed in that matches the financial lexicon and then adds them together, stores and returns the total of 1s added to financial_count
+
+        arguments: none
         """
         self.financial_count = sum([1 for word in self.sample_list
             if word in lexicon["domain"]["financial"]])
@@ -41,6 +45,8 @@ class Domainer:
     def behavioral_counter(self):
         """
         The behavioral_counter adds a 1 for each word in the list passed in that matches the behavioral lexicon and then adds them together, stores and returns the total of 1s added to behavioral_count
+
+        arguments: none
         """
         self.behavioral_count = sum([1 for word in self.sample_list
             if word in lexicon["domain"]["behavioral"]])
@@ -50,6 +56,8 @@ class Domainer:
     def scientific_counter(self):
         """
         The scientific_counter adds a 1 for each word in the list passed in that matches the scientific lexicon and then adds them together, stores and returns the total of 1s added to scientific_count
+
+        arguments: none
         """
         self.scientific_count = sum([1 for word in self.sample_list
             if word in lexicon["domain"]["scientific"]])
@@ -59,6 +67,8 @@ class Domainer:
     def educational_counter(self):
         """
         The educational_counter adds a 1 for each word in the list passed in that matches the educational lexicon and then adds them together, stores and returns the total of 1s added to educational_count
+
+        arguments: none
         """
         self.educational_count = sum([1 for word in self.sample_list
             if word in lexicon["domain"]["educational"]])
@@ -68,6 +78,8 @@ class Domainer:
     def political_counter(self):
         """
         The political_counter adds a 1 for each word in the list passed in that matches the political lexicon and then adds them together, stores and returns the total of 1s added to political_count
+
+        arguments: none
         """
         self.political_count = sum([1 for word in self.sample_list
             if word in lexicon["domain"]["politics"]])
@@ -76,6 +88,8 @@ class Domainer:
     def relationships_counter(self):
         """
         The relationships_counter adds a 1 for each word in the list passed in that matches the relationship lexicon and then adds them together, stores and returns the total of 1s added to relationships_count
+
+        arguments: none
         """
         self.relationships_count = sum([1 for word in self.sample_list
             if word in lexicon["domain"]["relationships"]])
@@ -84,6 +98,8 @@ class Domainer:
     def domain_decimal_math(self):
         """
         This function first checks to see if the total domain points is at zero, if so, it creates a dictionary where the keys are the different domains and the values are set to zero. Else, it takes the total from from each counter and divides that number by the total of ALL of the counters and returns the final domain values as a decimal
+
+        arguments: none
         """
         if self.total_domain_points == 0:
             self.final_domain_values = {
@@ -109,6 +125,8 @@ class Domainer:
     def find_highest_domain(self):
         """
         This function is getting the domain with the highest count and storing and returning the answer to self.final_domain_values.
+
+        arguments: none
         """
         self.highest_domain = max(self.final_domain_values.keys(), key=(lambda key: self.final_domain_values[key]))
         return self.highest_domain
@@ -116,11 +134,15 @@ class Domainer:
     def give_me_highest_domain(self):
         """
         This function simply gives us access to the domain with the highest counter
+
+        arguments: none
         """
         return self.highest_domain
 
     def __str__(self):
         """
         This is the a string of all of the information that we need from this class Domainer and can be printed in the terminal when importing Domainer
+
+        arguments: none
         """
         return "this sentence has an domain count of: {0} financial, {1} behavioral, {2} scientific, {3} educational,  {4} politics, and {5} relationships".format(self.domain_decimals["financial"], self.domain_decimals["behavioral"], self.domain_decimals["scientific"], self.domain_decimals["educational"], self.domain_decimals["politics"], self.domain_decimals["relationships"])
